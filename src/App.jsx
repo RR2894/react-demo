70b81cb22d527d8f3ee7bd0765dbb6b548679128
Randolph <86254950+RR2894@users.noreply.github.com>
2024-03-25 14:44:52 +0200

    boxes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
 import './App.css'
 import Box from './Box'
+import { useState } from 'react'
 
 function App() {
+const [counter, setCounter] = useState(0)
+const [persons, setPersons] = useState([
+  {id:1, name: "Randolph", title: "CEO", location: "Helsinki"},
+  {id:2, name: "Holden", title: "CTO", location: "somewhere"},
+  {id:3, name: "Oberon", title: "something", location: "wherever"},
+]);
+
+const clickHandler = () => {
+  console.log('Foxes forever!!!');
+  alert('Foxes forever');
+};
+
+const clickIncCounter = () => {
+  setCounter(counter+1);
+};
+const clickDecCounter = () => {
+  setCounter(counter-1);
+};
+const clickCounterReset= () => {
+  setCounter(counter*0);
+};
+
   return (
     <>
-    <Box/>
-    <Box/>
-    <Box/>
+    <p>Counter: {counter}</p>
+    <button onClick={clickHandler}>Do NOT click me</button>
+    <button onClick={clickIncCounter}>Counter increase</button>
+    <button onClick={clickDecCounter}>Counter decrease</button>
+    <button onClick={clickCounterReset}>Reset Counter</button>
+    {persons.map((person) =>(
+    <Box
+      key={person.name}
+      name={person.name}
+      title={person.title}
+      location={person.location}
+    />
+    ))}
     </>
   )
 }
--- src/Box.jsx
+++ src/Box.jsx
@@ -1,13 +1,23 @@
 import './App'
+import { useState } from 'react';
 
-const Box = () => {
+const Box = (props) => {
     return(
         <div>
-            <h1>Name:</h1>
-            <p>Title:</p>
-            <p>Location:</p>
+            <h1>Name: {props.name}</h1>
+            <p>Title: {props.title}</p>
+            <p>Location: {props.location}</p>
         </div>
     )
+}
+
+function BoxList() {
+    const [boxes, setBoxes] = useState([
+        {id: 1, name: 'Randolph'},
+        {id: 2, name: 'someone'},
+        {id: 3, name: 'someone_else'}
+    ]);
+
 }
 
 export default Box;
