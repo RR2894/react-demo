29eb2b09d245f96766b8e9155f90d524fb207595
Randolph <86254950+RR2894@users.noreply.github.com>
2024-03-25 11:10:11 +0200

    created Box components

---
+++ src/Box.jsx
@@ -0,0 +1,13 @@
+import './App'
+
+const Box = () => {
+    return(
+        <div>
+            <h1>Name:</h1>
+            <p>Title:</p>
+            <p>Location:</p>
+        </div>
+    )
+}
+
+export default Box;
